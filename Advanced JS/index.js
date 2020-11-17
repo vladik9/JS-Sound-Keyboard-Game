5263418589c1d3a0dc59d757500cdeda31083858
Cornici Vlad <49037796+vladik9@users.noreply.github.com>
2020-11-17 14:39:54 +0200

    first_submmit
    JS game whit sounds an buttons

---
+++ Advanced JS/index.js
@@ -0,0 +1,59 @@
+
+
+var nr_of_buttons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < nr_of_buttons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var my_button = this.innerHTML;
+        console.log(my_button);
+        play_my_sound(my_button);
+    })
+}
+
+document.addEventListener("keypress", function (event) {
+    play_my_sound(event.key);
+});
+
+
+function play_my_sound(key) {
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var tom5 = new Audio("sounds/snare.mp3");
+            tom5.play();
+            break;
+        case "k":
+            var tom6 = new Audio("sounds/crash.mp3");
+            tom6.play();
+            break;
+        case "l":
+            var tom7 = new Audio("sounds/kick-bass.mp3");
+            tom7.play();
+            break;
+        default:
+            alert("Missing input! Are you crazy?")
+            break;
+    }
+}
+
+
+
+
+
